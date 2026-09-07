7b781ca69b51a7752214fe23b3f90057717b23e1
Enisa Sabo <87073992+saboenisa@users.noreply.github.com>
2026-09-07 13:23:31 +0200

    Update filter_security_events.ps1

--- scripts/filter_security_events.ps1
+++ scripts/filter_security_events.ps1
@@ -1,1 +1,7 @@
+# Filter key security events
+$importantEvents = @(4624, 4625, 4672, 4688)
+
+Get-WinEvent -LogName Security |
+Where-Object { $_.Id -in $importantEvents } |
+Select-Object TimeCreated, Id, LevelDisplayName, Message
 
